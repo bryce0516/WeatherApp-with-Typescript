0281719060611ca70ca55e12a1059129199de108
nelv13 <nelv13@ibizsoftware.net>
2021-01-21 15:32:17 +0900

    create project with tsconfig

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import Styled from 'styled-components'
+
+interface Props {
+
+}
+const App = ({}: Props) => {
+  return (
+    <>
+      
+    </>
+  );
+};
+
+
+export default App;
